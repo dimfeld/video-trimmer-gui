e09664b422261ef6bc24730a3d2cfa2146f87a0c
Daniel Imfeld <daniel@danielimfeld.com>
2020-05-02 15:44:02 -1000

    Pointer cursor on textfield clear button

--- src/App.svelte
+++ src/App.svelte
@@ -110,7 +110,7 @@
   });
 
   const xIcon = `<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M6 18L18 6M6 6l12 12"></path></svg>`;
-  const rightIconClasses = "text-gray-400 rounded-lg transition-colors duration-200 ease-in hover:bg-gray-200"
+  const rightIconClasses = "cursor-pointer text-gray-400 rounded-lg transition-colors duration-200 ease-in hover:bg-gray-200"
 
 </script>
 
